9367baee5e2f8017d940609b62a03c7c5fc01cb1
Simon Philip <simonphilip@Simons-MacBook-Pro.local>
2025-02-28 03:27:45 +0530

    add default caching behaviour

--- infra/main.tf
+++ infra/main.tf
@@ -21,13 +21,35 @@ resource "aws_cloudfront_distribution" "cdn" {
     domain_name = aws_s3_bucket.website.bucket_regional_domain_name
     origin_id   = "S3Origin"
 
-    s3_origin_config {
-      origin_access_identity = ""
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
   enabled             = true
   default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl               = 0
+    default_ttl           = 3600
+    max_ttl               = 86400
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
 
   restrictions {
     geo_restriction {
@@ -41,5 +63,5 @@ resource "aws_cloudfront_distribution" "cdn" {
 }
 
 output "website_url" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+  value = "https://${aws_cloudfront_distribution.cdn.domain_name}"
 }
