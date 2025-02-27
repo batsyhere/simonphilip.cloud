02b4af0063c0d00eb825205dde40139a5215fd7a
Simon Philip <simonphilip@Simons-MacBook-Pro.local>
2025-02-28 03:25:10 +0530

    update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,37 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_s3_bucket" "website" {
   bucket = "simonphilip.cloud"
-  acl    = "public-read"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
   }
 }
 
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = "s3-origin"
+    origin_id   = "S3Origin"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
   }
 
-  enabled = true
+  enabled             = true
+  default_root_object = "index.html"
 
-  default_cache_behavior {
-    target_origin_id       = "s3-origin"
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
   }
 
